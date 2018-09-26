775104429194b1365db5ebf0dc24e5cf72b57d5a
James Zhan <zhiqiangzhan@gmail.com>
2018-09-26 20:22:05 +0800

    add syscall fork and getpid

--- syscall/syscall_map.py
+++ syscall/syscall_map.py
@@ -15,13 +15,13 @@ SYSCALL_MAP = mod.SYSCALL_MAP
 libc = ctypes.CDLL("libc.so.6")
 
 
-def syscall(entry, args):
+def syscall(entry, *args):
     return libc.syscall(SYSCALL_MAP[entry], *args)
 
 
 def do_op(name, *args):
     print(name, end=': ')
-    print(syscall(args[0], args[1:]))
+    print(syscall(*args))
 
 
 s = b"Hello World!\n"
@@ -31,6 +31,14 @@ do_op('PID', 'getpid')
 do_op('Thread ID', 'gettid')
 do_op('UID', 'getuid')
 do_op('GID', 'getgid')
+
+pid = syscall('fork')
+
+if pid == 0:
+    print("In Child pid is {}".format(syscall("getpid")))
+else:
+    print("In parent {} and child pid is {}.".format(syscall("getpid"), pid))
+
 
 
 
